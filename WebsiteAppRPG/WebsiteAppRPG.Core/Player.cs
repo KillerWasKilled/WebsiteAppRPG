32d5b1074dd57ee5707ca4450cec0cce3460be09
KillerWasKilled <fabry.samuellauder@gmail.com>
2026-01-10 20:28:23 +0100

    Entity Push :D

---
+++ WebsiteAppRPG/WebsiteAppRPG.Core/Player.cs
@@ -0,0 +1,8 @@
+﻿namespace WebsiteAppRPG.WebsiteAppRPG.Core
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public PlayerColors Colors { get; set; }
+    }
+}
---
+++ WebsiteAppRPG/WebsiteAppRPG.Core/PlayerColors.cs
@@ -0,0 +1,10 @@
+﻿namespace WebsiteAppRPG.WebsiteAppRPG.Core
+{
+    public class PlayerColors
+    {
+        public byte PlayerRedColor { get; set; }
+        public byte PlayerGreenColor { get; set; }
+        public byte PlayerBlueColor { get; set; }
+        public int PlayerAlphaLevel { get; set; }
+    }
+}
